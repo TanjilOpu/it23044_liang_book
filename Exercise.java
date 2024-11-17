b4c82de9bb40500d7885ad9df9051bd769e6cf76
Md. Tanjil Islam Opu <tanjilislamopu468@gmail.com>
2024-11-17 23:04:56 +0600

    Add files via upload

---
+++ Exercise.java
@@ -0,0 +1,15 @@
+
+package exercise;
+
+public class Exercise {
+  
+    public static void main(String[] args) {
+         double distance,hour,speed;
+        distance = 14/1.6;        // distance in mile
+        hour = (45*60+30)/3600.0;      // time in hour
+        speed = distance/hour;      //speed in mile per hour
+        System.out.println("Speed = "+speed);
+       
+    }
+    
+}
