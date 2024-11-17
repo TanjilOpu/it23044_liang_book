0633202496a2da3695194d139f88bc98f75d10a1
Md. Tanjil Islam Opu <tanjilislamopu468@gmail.com>
2024-11-17 23:29:53 +0600

    Update Exercise_eleven.java

--- Exercise_eleven.java
+++ Exercise_eleven.java
@@ -3,7 +3,7 @@ package exercise;
 
 
 public class Exercise_eleven {
-    public static void main(String[] args) {
+    public static void main(String[] args) {             // find the root of an equation 
         double a,b,c,d,x,y,e,f;
         a=3.4; b=50.2; e= 44.5;
         c=2.1; d = .55; f = 5.9;
