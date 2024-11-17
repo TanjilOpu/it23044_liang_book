ab7c38af766e33219423bd7313aa24f8be38119e
Md. Tanjil Islam Opu <tanjilislamopu468@gmail.com>
2024-11-17 22:27:58 +0600

    Add files via upload

---
+++ For_loop.java
@@ -0,0 +1,16 @@
+
+package for_loop;
+
+
+public class For_loop {
+
+  
+    public static void main(String[] args) {
+       for(int i=0;i<5;i++)
+       {
+           
+           System.out.println("Welcome to java");
+       }
+    }
+    
+}
