fa415ccaa121dd999e4351bb82569135ac3796d7
Md. Tanjil Islam Opu <tanjilislamopu468@gmail.com>
2024-11-17 23:29:11 +0600

    Add files via upload

---
+++ Exercise_eleven.java
@@ -0,0 +1,17 @@
+
+package exercise;
+
+
+public class Exercise_eleven {
+    public static void main(String[] args) {
+        double a,b,c,d,x,y,e,f;
+        a=3.4; b=50.2; e= 44.5;
+        c=2.1; d = .55; f = 5.9;
+        x = (e*d-b*f)/(a*d-b*c);
+        y = (a*f-e*c)/(a*d-b*c);
+        System.out.println("x = "+x);
+        System.out.println("y = "+y);
+        
+    }
+    
+}
