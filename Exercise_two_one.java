82cc0c385c2cfff86735e1ac8257bc667213e03f
Md. Tanjil Islam Opu <tanjilislamopu468@gmail.com>
2024-11-18 00:01:45 +0600

    Add files via upload

---
+++ Exercise_two_one.java
@@ -0,0 +1,17 @@
+
+package exercise;
+
+import java.util.Scanner;
+
+
+public class Exercise_two_one {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        double celsius;
+        System.out.println("Enter the tempereture in celcius scale ");
+        celsius = input.nextDouble();
+        double fahrenheit = (9.0/5)*celsius+32;
+        System.out.println(celsius + " Celsius is "+fahrenheit+" Fahrenheit");
+    }
+    
+}
