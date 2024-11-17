b0bdaed92dee2feb37e44a3065ad0919a91e77bf
Md. Tanjil Islam Opu <tanjilislamopu468@gmail.com>
2024-11-17 20:50:25 +0600

    Add files via upload

---
+++ Exercise1.java
@@ -0,0 +1,11 @@
+
+package exercise1;
+
+public class Exercise1 {
+
+   
+    public static void main(String[] args) {
+        System.out.println("Welcome to java\nWelcome to Computer science\nProgramming is fun");
+    }
+    
+}
---
+++ Exercise2.java
@@ -0,0 +1,14 @@
+
+package exercise1;
+import java.util.Scanner;
+
+public class Exercise2 {
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        int number;
+        number=input.nextInt();
+        System.out.println("number = "+number);
+    }
+    
+}
